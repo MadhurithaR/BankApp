ac1e2edb830163e602b6ed2d00b66d2d09c212e8
madhu <madhu@DESKTOP-FG8FL5O>
2018-04-07 11:41:42 -0700

    enum menu

---
+++ BankApplication/Account.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApplication
+{
+    enum TypeOfAccount
+    {
+        Savings,
+        Checking,
+        InstantAccess,
+        CD,
+        Loan
+    }
+    class Account
+    {
+        private static int lastAccountNumber = 0;
+
+        #region Properties
+
+        public int AccountNumber  { get; private set; }
+        public string AccountName { get; set; }
+        public TypeOfAccount AccountType { get; set; }
+        public decimal Balance { get; private set; }
+        public string  EmailAddress { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public  Account()
+        {
+            AccountNumber =  ++lastAccountNumber;
+            CreatedDate = DateTime.UtcNow;
+        }
+
+        //public Account(string accountName) : this()
+        //{
+        //   AccountName = accountName;
+        //}
+
+        //public Account(string accountName,string accountType) : this(accountName)
+        //{
+        //    AccountType = accountType;
+        //}
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Deposits the given amount
+        /// </summary>
+        /// <param name="amount"></param>
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        /// <summary>
+        /// Withdraw a given amount of money and return balance
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <returns></returns>
+        public decimal Withdraw(decimal amount)
+        {
+            Balance -= amount;
+            return Balance;
+        }
+
+        #endregion
+    }
+}
---
+++ BankApplication/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  // Object or instance
+            //  //  var account = new Account();
+            //  var account = new Account { AccountName = "my checking", AccountType = "Checking", EmailAddress = "test@test.com"};
+            //  account.AccountName = "My Checking";
+            //  account.Deposit(100.45M);
+            //// // account.Balance = 1000;
+            ////  Console.WriteLine("The amount before a deposit is {0}", account.Balance);
+            ////  account.deposit(100);
+            ////  Console.WriteLine("The amount after a deposited amount is {0}",account.Balance );
+            ////// account.Balance = account.withdraw(500);
+            ////  Console.WriteLine("The amount after a withdrawal amount is {0}", account.Balance);
+            //  Console.WriteLine($"AN: {account.AccountName}, CD: {account.CreatedDate}, Balance: {account.Balance},Name: {account.AccountName}, Type :{account.AccountType}");
+
+            //  var account2 = new Account();
+            //  Console.WriteLine($"AN: {account2.AccountName}, CD: {account2.CreatedDate}, Balance: {account2.Balance},Name: {account2.AccountName}, Type :{account.AccountType}");
+
+            Console.WriteLine("*********************");
+            Console.WriteLine("**Welcome to my bank!**");
+
+            while(true)
+            { 
+            Console.WriteLine("0. Quit");
+            Console.WriteLine("1. Create a new account");
+            Console.WriteLine("2. Deposit money");
+            Console.WriteLine("3. Withdraw Money");
+            Console.WriteLine("4. Print all accounts");
+
+            Console.WriteLine("Please select an option from above.");
+            var option = Console.ReadLine();
+
+                switch (option)
+                {
+                    case "0":
+                        Console.WriteLine("Thank you for visiting");
+                        return;
+                    case "1":
+                        Console.Write("Email Address: ");
+                        var email = Console.ReadLine();
+                        Console.Write("Account Name: ");
+                        var accountName = Console.ReadLine();
+                        Console.WriteLine("Account Type: ");
+                        var accountTypes = Enum.GetNames(typeof(TypeOfAccount)); // GetNames gives an array of the enum
+                        for (var i = 0; i < accountTypes.Length; i++)// you can also increment as i+=2 or how ever u want to increment
+                        {
+                            Console.WriteLine($"{i} . {accountTypes[i]}");
+                        }
+                        Console.Write("Your choice: ");
+                        var accountType = (TypeOfAccount)Enum.Parse(typeof(TypeOfAccount), Console.ReadLine());
+                        Console.Write("Initial Deposit: ");
+                        var amount = Convert.ToDecimal(Console.ReadLine());
+
+                        var account = new Account
+                        {
+                            AccountName = accountName,
+                            AccountType = accountType,
+                            EmailAddress = email,
+                        };
+                        if (amount > 0)
+                        {
+                            account.Deposit(amount);
+                        }
+                        Console.WriteLine($"AN: {account.AccountName}, CD: {account.CreatedDate}, Balance: {account.Balance},Name: {account.AccountName}, Type :{account.AccountType}");
+
+                        break;
+                }
+
+                //case "2":
+                //case "3":
+                //case "4":
+                //case "5":
+            }
+
+
+
+
+
+        }
+    }
+}
